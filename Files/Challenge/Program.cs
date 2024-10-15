d592224773efd07bd4e55a2ca6c27c9d6e9d85b8
hilmi gabi <gabihilmi4@gmail.com>
2024-10-15 07:29:10 +0100

    commit

---
+++ Files/Challenge/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+string firstpath = "FileCollection";
+string secondpath = "Results.txt";
+
+string ext;
+
+bool iscompatibleext(ext)
+{
+    return ext == ".docx" || ext == ".xlsx" || ext == ".pptx";
+}
+
+int doccount, totalcount, pptcount, xlscount;
+long docsize, totalsize , pptsize; xlssize;
+
+DirectoryInfo directoryInfo = new DirectoryInfo(firstpath);
+
+foreach(FileInfo fileInfo in directoryInfo.GetFiles())
+{
+    if (iscompatibleext(fileInfo.Extension()){
+        totalcount++;
+        totalsize = fileInfo.Length;
+
+        if (fileInfo.Extension == ".docx")
+        {
+            doccount++;
+            docsize = fileInfo.Length;
+
+
+        }
+        if (fileInfo.Extension == ".xlsx")
+        {
+            xlscount++;
+            xlssize = fileInfo.Length;
+        }
+        if (fileInfo.Extension == ".pptx")
+        {
+            pptcount++;
+            pptsize = fileInfo.Length;
+        }
+    }
+
+
+
+}
+
+using (StreamWriter sw = new StreamWriter(secondpath))
+{
+    sw.WriteLine("print all the results like count and size");
+}
--- Files/FileInfo/Program.cs
+++ Files/FileInfo/Program.cs
@@ -7,11 +7,15 @@ const string filename = "TestFile.txt";
 if (!File.Exists(filename)) {
     using (StreamWriter sw = File.CreateText(filename)) {
         sw.WriteLine("This is a text file.");
+        sw.WriteLine("This is a text file in a database in factoryreset");
+        
     }
 }
 
 // TODO: Get some information about the file
-
+Console.WriteLine(File.GetCreationTime(filename));
+Console.WriteLine(File.GetLastWriteTime(filename));
+Console.WriteLine(File.GetLastAccessTime(filename));
 
 // TODO: We can also get general information using a FileInfo 
 
